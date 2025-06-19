02fc0498e5fa81f03471cccee6282a6288d01adb
Welpguyyy <zhient123@gmail.com>
2025-06-19 08:36:34 +0800

    added security to authentication

--- src/app/homePage/page.tsx
+++ src/app/homePage/page.tsx
@@ -1,8 +1,23 @@
 'use client';
 import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/navigation';
+import { useEffect } from 'react';
+import { signOut } from "next-auth/react";
+
+
 
 export default function DashboardPage() {
+  
   const { data: session, status } = useSession();
+
+const router = useRouter();
+
+  useEffect(() => {
+    if (status === "unauthenticated") {
+      router.push("/");
+    }
+  }, [status, router]);
+
 
   if (status === "loading") {
     return null; // Or show a loading spinner
@@ -15,6 +30,8 @@ export default function DashboardPage() {
   return (
     <div>
       <h1>Welcome, {session.user?.name}!</h1>
+
+      <button onClick={() => signOut()}>Sign Out</button>
     </div>
   );
 }
