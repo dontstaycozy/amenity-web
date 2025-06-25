79bfb98f38e435789053d29621c82e1a34682df3
cozysharkmurks <wilfredalternate6969@gmail.com>
2025-06-26 01:48:11 +0800

    udpate to 1.8
    edited a bit sa supabaseclient.ts

--- src/app/lib/supabaseclient.ts
+++ src/app/lib/supabaseclient.ts
@@ -4,6 +4,4 @@ import { createClient } from "@supabase/supabase-js";
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL!;
 const supabaseAnonKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
 
-const supadata = createClient(supabaseUrl, supabaseAnonKey);
-
-export  default  supadata;
+export const supabase = createClient(supabaseUrl, supabaseAnonKey);
