c1f5af63d19c6236554d3cd405643627df1b6a31
Welpguyyy <zhient123@gmail.com>
2025-07-07 08:46:23 +0800

    Update next-auth.d.ts

--- src/types/next-auth.d.ts
+++ src/types/next-auth.d.ts
@@ -6,13 +6,12 @@ declare module "next-auth" {
     user:{
     id: string;
     name:string;
-    email:string;
-      image_url?: string | null;
+      
   }
 }
 
   interface user {
     id: string;
-      image_url?: string | null;
+   
   }
 }
